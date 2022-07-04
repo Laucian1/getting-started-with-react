ae1a016ce5528f6d8c414bcaa0e6d2c688c32ca3
Russell Smith <resmithjr88@gmail.com>
2022-07-04 13:36:22 -0600

    Lists and Tables

--- src/App.js
+++ src/App.js
@@ -2,16 +2,29 @@ import "./App.css";
 import WelcomeBack from "./WelcomeBack"
 import Greeting from "./Greeting"
 
+const toDoList = [
+  { completed: false, description: "Make the bed" }, 
+  { completed: true, description: "Wash the dishes" }, 
+  { completed: false, description: "Mow the lawn" }
+]
+
 function App() {
+  const rows = toDoList.map(({completed, description}, index) => (
+    <tr key = {index}>
+      <td>{description}</td>
+      <td>{completed ? "Yes" : "No"}</td>
+    </tr>))
+
   return (
-    <>
-      <WelcomeBack name="Joe"/>
-      <WelcomeBack name="Anna"/>
-      <WelcomeBack />
-      <Greeting />
-      <Greeting language="es" />
-      <Greeting language="fr" />
-    </>
+    <table>
+      <thead>
+        <tr>
+          <th>Description</th>
+          <th>Completed?</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
   )
 }
 
