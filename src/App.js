6b9d6083e705dc047ebb68b951ea56b021cd36b4
Russell Smith <resmithjr88@gmail.com>
2022-07-04 14:35:09 -0600

    Styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import React from "react"
 import "./App.css";
 import WelcomeBack from "./WelcomeBack"
+import Quote from "./Quote"
+
+const quote = {
+  text:
+    "I am a great believer in luck, and I find the harder I work, the more I have of it.",
+  author: "Thomas Jefferson",
+}
 
 function App() {
   return (
     <>
       <WelcomeBack name="Joe"/>
-      <WelcomeBack name = "Anna"/>
-      <WelcomeBack />
+      <Quote quote={quote}/>
     </>
   )
 }
--- src/WelcomeBack.js
+++ src/WelcomeBack.js
@@ -1,7 +1,22 @@
-import "./WelcomeBack.css"
+import React from "react"
 
 function WelcomeBack({ name = "valued customer" }) {
-    return <p>Welcome back, {name}!</p>
+    const pStyle = {
+        color: "black",
+        fontFamily: "Zen dots, cursive",
+    }
+    const userName = {
+        padding: "2px 4px",
+        marginRight: "2px",
+        backgroundColor: "#000",
+        color: "#fff",
+        borderRadius: "4px",
+    }
+    return (
+        <p style={pStyle}>
+            Welcome back, <span style={userName}>{name}</span>!
+        </p>
+    )   
 }
 
 export default WelcomeBack
